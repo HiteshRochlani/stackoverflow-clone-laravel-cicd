7ca90672c028029d990092b4da03ee1bc0b1a0c2
Hitesh2004 <hiteshrochlani2004@gmail.com>
2025-03-04 19:03:56 +0530

    chore: dockerized binary

---
+++ build/app-entrypoint.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+chown -R $USER:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+find storage bootstrap/cache -type f -exec chmod 664 {} \;
+php-fpm -y /usr/local/etc/php-fpm.conf -R
