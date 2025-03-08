be64b6bf0d03ea0a926b52029c17889c2d0dc44d
Hitesh2004 <hiteshrochlani2004@gmail.com>
2025-03-08 13:49:13 +0000

    chore: changed nginx user to www-data

--- build/app-entrypoint.bash
+++ build/app-entrypoint.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R $USER:www-data storage bootstrap/cache
+chown -R $USER:www-data storage bootstrap/cache public
 chmod -R 775 storage bootstrap/cache
 find storage bootstrap/cache -type f -exec chmod 664 {} \;
 php-fpm -y /usr/local/etc/php-fpm.conf -R
